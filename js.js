76754f6a48207c530ad5e491467536a5d202e482
angelakadow <30486448+angelakadow@users.noreply.github.com>
2018-08-24 10:15:13 -0400

    Add files via upload
    Change dropdown menu to click

--- js.js
+++ js.js
@@ -170,3 +170,53 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function menu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+$("#menu").on("click", function(){
+    menu();
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function about() {
+    document.getElementById("about").classList.toggle("show");
+}
+
+$("#aboutMenu").on("click", function(){
+    about();
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menu')) {
+
+    var dropdowns = document.getElementsByClassName("dropdwn-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
