30b0a9cff4ebc2b064c4d3b2ac4a1700b7d58b75
angelakadow <30486448+angelakadow@users.noreply.github.com>
2021-04-06 21:00:36 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,23 @@
 */
 (function() {
   "use strict";
+  
+  //number of years in About me section
+  var date1, date2;  
+         //define two date object variables with dates inside it  
+         date1 = new Date("01/01/2017");  
+         date2 = new Date();  
+  
+         //calculate time difference  
+         var time_difference = date2.getTime() - date1.getTime();  
+  
+         //calculate days difference by dividing total milliseconds in a day  
+         var days_difference = time_difference / (1000 * 60 * 60 * 24);  
+         var years_difference = days_difference / 365;
+           
+         console.log(Math.round(years_difference));
+
+         $('#years').append(Math.round(years_difference));
 
   /**
    * Easy selector helper function
